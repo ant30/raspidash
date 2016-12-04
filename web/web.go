0da0bf95daefeb56fbd8fcefedc08c57da3b8585
ant30 <ant30tx@gmail.com>
2016-12-04 19:17:46 +0100

    Using basic template and layout for index

--- web/web.go
+++ web/web.go
@@ -1,13 +1,23 @@
 package web
 
 import (
-	"io"
 	"log"
+    "html/template"
 	"net/http"
 )
 
 func renderIndex(w http.ResponseWriter) {
-	io.WriteString(w, "Simple Index page for raspidash project")
+	t, err := template.ParseFiles("templates/layout.html", "templates/index.html")
+    if err != nil {
+        log.Println("Error loading the templates")
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+    err2 := t.Execute(w, nil)
+    if err2 != nil {
+        log.Println("Error rendering the templates")
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
