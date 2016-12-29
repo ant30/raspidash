80a0b1b90d3b0e2730f2dc35ad507582896a68ef
ant30 <ant30tx@gmail.com>
2016-12-29 19:31:52 +0100

    Web interface http port configurable by cli

--- main.go
+++ main.go
@@ -1,10 +1,25 @@
-
 package main
 
 import (
+    "flag"
+    "log"
+    "os"
     "github.com/ant30/raspidash/web"
 )
 
+var (
+)
+
 func main() {
-    web.WebServer()
+    if len(os.Args) < 2 {
+        log.Println("a subcommand is required [server]")
+        os.Exit(1)
+    }
+    switch os.Args[1] {
+        case "server":
+            web.WebServer(os.Args[2:])
+        default:
+            flag.PrintDefaults()
+            os.Exit(1)
+    }
 }
--- web/web.go
+++ web/web.go
@@ -1,14 +1,24 @@
 package web
 
 import (
+  "flag"
   "log"
   "net/http"
+
 )
 
+var (
+    serverCommand = flag.NewFlagSet("server", flag.ExitOnError)
+)
 
-func WebServer() {
-  http.HandleFunc("/", IndexHandler)
-  http.HandleFunc("/display/", DisplayHandler)
-  log.Println("Listening...")
-  log.Println(http.ListenAndServe(":8000", nil))
+func WebServer(args []string) {
+    serverListen := serverCommand.String("listen", ":8000", "This is the tcp port to bind the http server interface, like :8000 or 0.0.0.0:8000")
+    serverCommand.Parse(args)
+    log.Printf("Preparing to listen on %v \n", *serverListen)
+
+
+    http.HandleFunc("/", IndexHandler)
+    http.HandleFunc("/display/", DisplayHandler)
+    log.Println("Listening...")
+    log.Println(http.ListenAndServe(*serverListen, nil))
 }
