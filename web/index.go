543daedfcd182565156649ffd5f01282f154612b
ant30 <ant30tx@gmail.com>
2017-01-16 17:10:55 +0100

    Show the list of available devices in index

--- web/index.go
+++ web/index.go
@@ -3,16 +3,21 @@ package web
 import (
   "net/http"
   "log"
+  "github.com/ant30/raspidash/discovery"
 )
 
 type indexContext struct {
   Title string
+  DeviceList []discovery.ServiceDescription
 }
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
   log.Printf("%s: %s\n", r.Method, r.URL.Path)
   if r.Method == "GET" {
-    RenderTemplate(w, "templates/index.html", indexContext{"So what!"})
+    RenderTemplate(w, "templates/index.html", indexContext{
+        Title: "So what!",
+        DeviceList: discovery.ListDevices(),
+    })
   } else {
 	return
   }
